bebad9e0fd64069d5e00d60d58dca1e806a13a76
Rebecca Lima <rebeccammlima@gmail.com>
2026-04-30 19:22:17 -0300

    aa

--- excerciciosRepeticao.js
+++ excerciciosRepeticao.js
@@ -1,8 +1,100 @@
-//1
-
-for (let i = 1; i <= 10; i++) {
+console.log("Contagem:")
+for (let i = 0; i <= 10; i++) {
     console.log(i)
 }
 
-//2
+
+//tabuada de um número:
+
+
+const numero1 = Number(prompt("Tabuada de qual numero? "))
+if (Number.isNaN(numero1)) {
+    alert("Número invalido")
+} else {
+    console.log(`\n Tabuada do ${numero1}\n`)
+
+
+    for(let i = 0; i <= 10; i++){
+        const resultado = numero1 * i;
+        alert(`${numero1} x ${i} = ${resultado}`)
+    }
+}
+
+
+//Soma dos primeiros N números naturais:
+
+
+const num = Number(prompt("Digite um numero para a soma:"))
+let soma1 = 0
+for(let ii = 1; ii <=num; ii++){
+    soma1 += ii
+}
+alert(`soma de 1 até ${num} = ${soma1}`)
+
+
+//Exibir os números pares de 1 a 50:
+
+
+
+
+for (let index = 0; index <= 50; index++) {
+   
+    if (index % 2 === 0) {
+        console.log(index)
+    }
+}
+
+
+// Adivinhação
+const numeroSecreto = Math.floor(Math.random() * 10) +1;
+let palpite;
+let tentativa = 0;
+alert("Adivinhe um numero de 1-10");
+
+
+do {
+    palpite = Number(prompt("Seu palpite: "))
+    tentativa++
+    if (Number.isNaN(palpite)) {
+        alert("Digite apenas numeros válidos")
+    } else if (palpite < numeroSecreto) {
+        alert("Muito baixo, tente novamente")
+    }else if (palpite > numeroSecreto){
+        alert("muito alto")
+    }else {
+        alert(`Acertou! limpe a cueca! Numero de tentativas ${tentativa}`);
+    }
+} while (palpite !== numeroSecreto);
+
+
+console.log("Contagem regressiva");
+let tempo = 10;
+console.log("contagem regressiva em...");
+while (tempo > 0) {
+    console.log(tempo);
+    tempo--;
+}
+console.log("===========")
+console.log("Decolou!");
+
+
+//Soma dos dígitos de um número:
+
+
+let numero = Number(prompt("Digite um numero inteiro"))
+
+
+let soma = 0;
+
+
+while (numero > 0) {
+    let digito = numero % 10
+    soma += digito;
+
+
+    numero = Math.floor(numero/10);
+}
+
+
+alert("Soma dos digitos:" + soma)
 
---
+++ filter.js
@@ -0,0 +1,15 @@
+//filter
+
+const people = [
+    {name: "Lucas", age: 35},
+    {name: "João", age: 35},
+    {name: "Maria", age: 35},
+    {name: "Ana", age: 35},
+]
+
+//crie um array com vários números
+//use filtro para mostrar  apenas os números maiores que 50
+
+const num = [10,55,80,20,100];
+
+const maiores = numeroSecreto
